bd72613813741b645e68807c11cbf32d87085bb9
Hadi Rezaei <hadirezaei1377hr@gmail.com>
2026-06-22 17:43:14 +0330

    implement real estate transfer backen

--- tronbox-config.js
+++ tronbox-config.js
@@ -1,10 +1,13 @@
+require("dotenv").config();
+
 module.exports = {
   networks: {
     nile: {
       privateKey: process.env.PRIVATE_KEY,
       userFeePercentage: 100,
       feeLimit: 1000000000,
-      fullHost: "https://nile.trongrid.io"
+      fullHost: "https://nile.trongrid.io",
+      network_id: "*"
     }
   },
 
